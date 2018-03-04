5bc9a603e00aafff3f538309035ec871b1f5a77a
dipterix <dipterix.wang@gmail.com>
2018-03-04 02:08:20 -0600

    Added messages

--- R/hello.R
+++ R/hello.R
@@ -1,34 +1,18 @@
 # package to install RAVE
 
-.onAttach <- function(libname, pkgname){
+.onLoad <- function(libname, pkgname){
   # load RAVE
+
+
   pkgs = utils::installed.packages()[,1]
   if(!'rave' %in% pkgs){
-    # need_devtools = TRUE
-    # if('devtools' %in% pkgs){
-    #   ver = utils::packageVersion('devtools')
-    #   if(utils::compareVersion(
-    #     as.character(ver), '1.13.4'
-    #   ) >= 0){
-    #     need_devtools = FALSE
-    #   }
-    # }
-    #
-    # if(need_devtools){
-    #   opt = options('repos')
-    #   if(length(opt$repos)){
-    #     repos = opt$repos[1]
-    #   }else{
-    #     repos = "https://cloud.r-project.org"
-    #   }
-    #   install.packages('devtools', repos = repos)
-    # }
 
     # install rhdf5
     source("https://bioconductor.org/biocLite.R")
     bioc_p = c("rhdf5", "HDF5Array")
     bioc_p = bioc_p[!bioc_p %in% pkgs]
     if(length(bioc_p)){
+      message('Installing Bioconductor Dependencies...')
       biocLite(bioc_p, suppressUpdates = T, suppressAutoUpdate = T)
     }
 
@@ -39,7 +23,11 @@
       return('master')
     })->
       ref
-    devtools::install_github('beauchamplab/rave', ref = ref)
+
+
+    message('This is RAVE (', ref, ')')
+    message('Installing RAVE - this might take a while..... (10 Min?)')
+    devtools::install_github('beauchamplab/rave', ref = ref, quiet = F)
     do.call('require', args = list(
       package = 'rave',
       character.only = TRUE
