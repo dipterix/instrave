c8aa02e9822589d99791b8cd669dda3881f333ab
dipterix <dipterix.wang@gmail.com>
2020-01-26 08:46:39 -0600

    libpath and R path

--- rave-installer-windows.bat
+++ rave-installer-windows.bat
@@ -20,7 +20,35 @@ if exist %GITPATH% (
 )
 
 powershell -Command "Invoke-WebRequest %RAVEINSTSCRIPT% -OutFile %TEMP%\RAVE.sh"
-%GITPATH% %TEMP%\RAVE.sh
+
+SETLOCAL ENABLEEXTENSIONS
+SET RKEY=
+SET RPATH=
+FOR /F "tokens=* skip=2" %%L IN ('reg.exe QUERY HKLM\Software\R-core\R /f * /k ^| sort') DO (
+    IF NOT "%%~L"=="" SET "RKEY=%%~L"
+)
+IF NOT DEFINED RKEY (
+    ECHO Unable to locate registry key HKLM\Software\Rcore\R
+    EXIT /B 1
+)
+FOR /F "tokens=2* skip=2" %%A IN ('REG QUERY %RKEY% /v "installPath"') DO (
+    IF NOT "%%~B"=="" SET "RPATH=%%~B"
+)
+IF NOT DEFINED RPATH (
+    ECHO Unable to query registry value %RKEY%\installPath
+    EXIT /B 2
+)
+IF NOT EXIST "%RPATH%" (
+    ECHO Found path for R (%RPATH%^) does not exist
+    EXIT /B 3
+)
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    SET "PATH=%RPATH%\bin\x64;C:\Rtools\bin;%PATH%"
+) ELSE (
+    SET "PATH=%RPATH%\bin\i386;C:\Rtools\bin;%PATH%"
+)
+
+%GITPATH% --login %TEMP%\RAVE.sh
 
 pause
 
