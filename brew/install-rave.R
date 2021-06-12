08d808a2fc5c74848c9b29f344a8689c5c8226cb
dipterix <zw23@rice.edu>
2021-06-11 22:26:27 -0400

    added brew script

---
+++ brew/install-rave.R
@@ -0,0 +1,83 @@
+git_token <- "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"
+repos <- "https://cloud.r-project.org"
+# Check if remotes is installed
+if(!require(remotes)){
+  utils::install.packages(
+    pkgs = "remotes",
+    repos = repos,
+    clean = TRUE,
+    type = 'source'
+  )
+}
+if(!require(docopt)){
+  utils::install.packages(
+    pkgs = "docopt",
+    repos = repos,
+    clean = TRUE,
+    type = 'source'
+  )
+}
+
+# 
+# doc <- "Usage: install-rave [(BREW_PATH)]
+# 
+# -h --help                   show this help text
+# 
+# Required:
+#   BREW_PATH                 Brew path
+# 
+# Example: install-rave /opt/Homebrew
+# 
+# "
+# opt <- docopt::docopt(doc)
+# 
+# 
+# inst_github <- function(..., upgrade = 'always'){
+#   remotes::install_github(
+#     repo = c(...),
+#     upgrade = upgrade,
+#     repos = repos,
+#     type = 'binary',
+#     auth_token = git_token,
+#     dependencies = TRUE,
+#     Ncpus = parallel::detectCores()
+#   )
+# }
+# 
+# # Add brew to path /opt/homebrew/bin:/opt/homebrew/sbin
+# if(!length(opt$BREW_PATH) || !file.exists(file.path(opt$BREW_PATH, 'bin', 'brew'))){
+#   stop("Cannot find brew")
+# }
+# # locate h5cc
+# # /usr/local/Cellar/hdf5/
+# hdf5_dir <- file.path(opt$BREW_PATH, 'Cellar', 'hdf5')
+
+# # install hdf5 now -- #{HOMEBREW_PREFIX}/bin/h5cc
+# utils::install.packages(
+#   "hdf5r",
+#   repos = repos,
+#   clean = TRUE,
+#   type = 'source', 
+#   configure.args = sprintf("--with-hdf5=/opt/homebrew/bin/h5cc")
+# )
+
+# This will bugged out
+inst_github("beauchamplab/ravebuiltins@migrate2", "beauchamplab/rave")
+
+
+
+utils::install.packages(
+  "raveio",
+  repos = repos,
+  clean = TRUE,
+  type = 'source'
+)
+
+
+
+
+
+
+
+
+
---
+++ brew/recipe.sh
@@ -0,0 +1,26 @@
+
+cd /Users/dipterix/Dropbox/projects/instrave/brew
+
+# brew create --cask --set-name rave https://github.com/beauchamplab/rave/archive/refs/tags/v0.1.9-beta.tar.gz
+
+brew create --set-name rave-m1 https://github.com/beauchamplab/rave/archive/refs/tags/v0.1.9-beta.tar.gz
+
+brew create --cask --set-name r-m1 https://cran.r-project.org/bin/macosx/big-sur-arm64/base/R-4.1.0-arm64.pkg
+
+export HOMEBREW_NO_AUTO_UPDATE=1
+
+rm /opt/homebrew/Library/Taps/homebrew/homebrew-cask/Casks/rave.rb
+arch -arm64 brew install --verbose --debug --build-from-source rave
+
+brew install gcc
+
+Rscript --no-save "install-rave.R"
+
+brew test --cask rave
+
+/opt/homebrew/Library/Taps/homebrew/homebrew-cask/Casks/rave.rb
+
+sha256 "77dcf288dc4198ba32359fe5fc2a79303ba7bc960033086ff7856dcc318ce953"
+
+brew create --set-name rave https://github.com/beauchamplab/rave/archive/refs/tags/v0.1.9-beta.tar.gz
+/opt/homebrew/Library/Taps/homebrew/homebrew-core/Formula/rave.rb
