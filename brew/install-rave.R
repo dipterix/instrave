2bf587d86de1ccc0f658513bf5ae0e8554c8bfe4
dipterix <zw23@rice.edu>
2021-06-13 04:40:48 -0400

    does not require source installation

--- brew/install-rave.R
+++ brew/install-rave.R
@@ -5,7 +5,6 @@ if(!require(remotes)){
     pkgs = "remotes",
     repos = repos,
     clean = TRUE,
-    type = 'source'
   )
 }
 
@@ -14,51 +13,10 @@ inst_github <- function(..., upgrade = 'always'){
     repo = c(...),
     upgrade = upgrade,
     repos = repos,
-    type = 'source',
     dependencies = TRUE,
     Ncpus = parallel::detectCores()
   )
 }
-
-# if(!require(docopt)){
-#   utils::install.packages(
-#     pkgs = "docopt",
-#     repos = repos,
-#     clean = TRUE,
-#     type = 'source'
-#   )
-# }
-# 
-# doc <- "Usage: install-rave [(BREW_PATH)]
-# 
-# -h --help                   show this help text
-# 
-# Required:
-#   BREW_PATH                 Brew path
-# 
-# Example: install-rave /opt/Homebrew
-# 
-# "
-# opt <- docopt::docopt(doc)
-# 
-# 
-# 
-# # Add brew to path /opt/homebrew/bin:/opt/homebrew/sbin
-# if(!length(opt$BREW_PATH) || !file.exists(file.path(opt$BREW_PATH, 'bin', 'brew'))){
-#   stop("Cannot find brew")
-# }
-# # locate h5cc
-# # /usr/local/Cellar/hdf5/
-# hdf5_dir <- file.path(opt$BREW_PATH, 'Cellar', 'hdf5')
-
-# # install hdf5 now -- #{HOMEBREW_PREFIX}/bin/h5cc
-# utils::install.packages(
-#   "hdf5r",
-#   repos = repos,
-#   clean = TRUE,
-#   type = 'source', 
-#   configure.args = sprintf("--with-hdf5=/opt/homebrew/bin/h5cc")
-# )
 
 # This will bugged out
 inst_github("beauchamplab/rave")
